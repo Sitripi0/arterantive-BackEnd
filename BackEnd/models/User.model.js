15929000d9223b76b36af7be475a0728ab7a7799
Sitripi0 <216026207+Sitripi0@users.noreply.github.com>
2025-08-05 17:55:29 +0200

    Update : User.model

--- BackEnd/models/User.model.js
+++ BackEnd/models/User.model.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
     email: {
@@ -18,6 +17,20 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Name is required."],
     },
+    role: {
+      type: String,
+      enum: ["artist","user"],
+      default:"user",
+    },
+    bio: {type:String, required:[true, "Bio is required"]},
+    profileImage : {type: String},
+    socialMedia:[
+      {
+        platform:{type: String}, 
+        URL: {type: String}
+      }
+    ],
+    favorites:[{type : Schema.Types.ObjectId, ref:"User"}]
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
